3dc2b3383d44606734714dfb5a19e094db285b33
Jerry Ren <jerryren.5279@gmail.com>
2023-03-09 15:42:26 -0500

    Summary component now takes in data object

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { Hero } from "./components/Hero";
 import { Summary } from "./components/Summary";
 
+import data from "./data.json";
+import { ReactComponent as ReactionLogo } from "./assets/images/icon-reaction.svg";
+import { ReactComponent as MemoryLogo } from "./assets/images/icon-memory.svg";
+import { ReactComponent as VerbalLogo } from "./assets/images/icon-verbal.svg";
+import { ReactComponent as VisualLogo } from "./assets/images/icon-visual.svg";
+
+data[0].icon = <ReactionLogo />;
+data[1].icon = <MemoryLogo />;
+data[2].icon = <VerbalLogo />;
+data[3].icon = <VisualLogo />;
+
+console.log(data);
+
 function App() {
   return (
     <div className="App">
@@ -8,7 +21,7 @@ function App() {
         <div id="absolute-position-container">
           <div id="flex-container">
             <Hero />
-            <Summary />
+            <Summary data={data} />
           </div>
         </div>
       </main>
